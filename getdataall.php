511a035b24f1edcb88d5f1142f575a78509941d3
Heri Prastio <heri.prastio56@gmail.com>
2018-05-03 22:22:57 +0700

    Dengan cara berbeda

--- getdataall.php
+++ getdataall.php
@@ -7,7 +7,11 @@ $result = mysqli_query($con, $sql);
 $json_array = array();
 
 while ($row  = mysqli_fetch_assoc($result)) {
-    $json_array[] = $row;
+    array_push($json_array, array(
+        "Id No" => $row ['id'],
+        "Nama Mata Kuliah" => $row['nama'],
+        "Nama Kategori" => $row['category']
+    ));
 }
 echo json_encode($json_array);
 
